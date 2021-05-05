4d64c9cb75f1160b1ea5ab9d7a168045a2bd5b8c
404 <62575759+onlymachiavelli@users.noreply.github.com>
2021-05-05 06:35:38 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div>
+      {/* header */}
+      <Component {...pageProps} />
+    </div>
+  )
 }
 
 export default MyApp
